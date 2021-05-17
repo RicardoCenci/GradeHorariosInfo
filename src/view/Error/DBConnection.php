1dd1bf53dc6b307c33349c948b6005bbdba23308
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2021-05-17 17:03:42 -0300

    Error View

---
+++ src/view/Error/DBConnection.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+    <head>
+        <title><?=$data['title']?></title>
+        <meta charset='UTF-8'>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel='stylesheet' href='./View/Resources/home/style.css'>
+    </head>
+        <body class="container-fluid">
+        Ops... Algo deu errado na comunicação com o banco de dados!
+        Por favor contate-nos pelo email
+        </body>
+    <script src='.js'></script>
+</html>
