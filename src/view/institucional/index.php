6ee2f95fae32bece81ab06677a643765331ba024
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2021-05-21 19:16:38 -0300

    Drag and drop interface for horarios

--- src/view/institucional/index.php
+++ src/view/institucional/index.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="<?=url('/src/view/institucional/index.css')?>">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="<?=url('/src/view/institucional/js/index.js')?>" type='module' defer></script>
+    <link href="https://fonts.googleapis.com/css2?family=Newsreader&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -20,20 +21,47 @@
     <main id='root'>
         <h1 id='appTittle'>Horarios</h1>
         <section id='app'>
-            <div id="horarios">
+            <!-- <div id="horarios">
             <div class='gridItem'>
+                    <p title='Ordem'>O</p>
+                    <p title='Horario de Entrada' style='padding:5px'>Entrada</p>
+                    <p title='Horario de Saida'>Saida</p>
+                </div>
+            </div> -->
+            <div class="actions">
+                <h1>Ações</h1>
+                <fieldset>
+                    <label for="edit">Editar Horario</label>
+                    <div id="edit" active></div>
+                </fieldset>
+                <fieldset>
+                    <label for="ordem">Editar ordem dos horarios</label>
+                    <div id="ordem"></div>
+                </fieldset>
+
+            </div>
+            <ul id='horarios' data-mode='edit'>
+                <li>
                     <p>ID</p>
                     <p>Entrada</p>
                     <p>Saida</p>
-                </div>
-            </div>
+                </li>
+            </ul>
         </section>
     </main> 
-    <div id="modal" active>
-        <div class="modalContent" active>
+
+
+
+    <div id="modal">
+        <div class="modalContent">
             <div id='wrapper'>
                 <h1>Horario</h1>
                 <div>
+                    <fieldset>
+                        <label for="exitTime">ID</label>
+                        <input type="text" id='ID' disabled>
+                    </fieldset>
+
                     <fieldset>
                         <label for="entranceTime">Entrada</label>
                         <input type="text" id='entranceTime'>
@@ -53,8 +81,20 @@
                 </fieldset>
                 </div>
                 <div id="confirmAction">
-                    are u sure?
-                    <button id="cancelConfirm">Leave</button>
+                    <div class="actionIconContainer">
+                        <div class="actionIcon">!</div>
+                    </div>
+                    <div class="actionConfirmText">
+                        <h1>Você tem certeza?</h1>
+                        <p class='actionConfirmSub'>Essa ação não podera ser revertida</p>
+                        <p>Excluir <span id='deletePreview'>Horario 5: 12:00 ate 13:30</span>?</p>
+                    </div>
+                    <fieldset class='buttonGroup'> 
+                        <div>
+                            <button id="ActionConfirmButton" class='button danger'>Excluir</button>
+                            <button id="ActionCancelButton" class='button cancel'>Voltar</button>
+                        </div>
+                    <fieldset>
                 </div>
             </div>
     </div>
--- src/view/institucional/js/Modal.js
+++ src/view/institucional/js/Modal.js
@@ -7,32 +7,66 @@ export default class Modal{
 
     entranceInput = document.getElementById('entranceTime')
     exitInput = document.getElementById('exitTime')
+    idInput = document.getElementById('ID')
 
-    confirmModalBackButton = document.getElementById('cancelConfirm')
+    confirmModalBackButton = document.getElementById('ActionCancelButton')
     contentRoot = document.querySelector('.modalContent')
+
+    ActionConfirmButton = document.getElementById('ActionConfirmButton')
+    deletePreview = document.getElementById('deletePreview')
+    
+    #currElement;
+    #confirmActionPromise;
+
     constructor(){
         this.modalRoot.addEventListener('click',this.close)
         this.addBtn.addEventListener('click',this.saveAction)
         this.deleteBtn.addEventListener('click',this.deleteAction)
 
         this.confirmModalBackButton.addEventListener('click',this.openConfirm)
-
+        this.ActionConfirmButton.addEventListener('click',this.#confirmAction)
     }
     open = (e)=>{
         const elementDataset = e.currentTarget.dataset
         this.entranceInput.value = elementDataset.entrance
         this.exitInput.value = elementDataset.exit
+        this.idInput.value = elementDataset.id
+        this.#currElement = {
+            'id':elementDataset.id,
+            'entrance': elementDataset.entrance,
+            'exit': elementDataset.exit
+        }
         this.modalRoot.toggleAttribute('active')
     }
     close = (e)=>{
         if (e.target.id == 'modal' || e.target.id == 'cancel') {
             this.modalRoot.toggleAttribute('active')
+            this.#currElement = {}
         }
     }
+    
     deleteAction = (e)=>{
-        this.openConfirm();
+        this.deletePreview.innerText = `Horario ${this.#currElement.id}: ${this.#currElement.entrance} ate ${this.#currElement.exit}`
+        this.#setConfirmPromise();
+        this.openConfirm().then(e=>{
+            console.log(this.#currElement);
+        });
     }
     openConfirm = (e)=>{
         this.contentRoot.toggleAttribute('active')
+        return this.#confirmActionPromise
+    }
+
+    #setConfirmPromise(){
+        let rej,res
+        this.#confirmActionPromise = new Promise((resolve,reject)=>{
+            rej = reject
+            res = resolve
+        })
+        this.#confirmActionPromise.resolve = res;
+        this.#confirmActionPromise.reject = rej;
+    }
+    #confirmAction = (e)=>{
+        this.#confirmActionPromise.resolve()
     }
 }
